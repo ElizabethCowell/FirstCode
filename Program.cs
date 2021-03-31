01ce5b4413023e5a86d8ce9e5250625d5aed0306
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-03-31 15:01:03 -0400

    first code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            var age = int.Parse(Console.ReadLine());
+            if (age > 20){
+                Console.WriteLine("You can drink!");
+            }
+            else{
+                Console.WriteLine("Sorry, no drinks for you!");
+            }
+
+        }
+    }
+}
